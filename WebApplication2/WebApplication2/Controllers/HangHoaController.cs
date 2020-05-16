7fc0620bccb0a71291e3b6301a13af31e47126b9
NN <kidokawr@gmail.com>
2020-05-16 15:30:30 +0700

    buoi02

---
+++ WebApplication2/WebApplication2/Controllers/HangHoaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var hh = new HangHoa
+            {
+                DonGia = 1,
+                TenHangHoa = "aa"
+            };
+            return View(hh);
+        }
+
+        public IActionResult DanhSach() 
+        {
+            List<HangHoa> danhsach = new List<HangHoa>();
+            HangHoa tmp;
+
+            var rd = new Random();
+            var SoPhanTu = rd.Next(3, 10);
+
+            for (int i = 0; i < SoPhanTu; i++)
+            {
+                tmp = new HangHoa
+                {
+                    TenHangHoa = $"Samsung {rd.Next()}",
+                    DonGia = rd.NextDouble() * 10,
+                };
+                danhsach.Add(tmp);
+            }
+
+            return View(danhsach);
+        }
+    }
+}
